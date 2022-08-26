0dde772eacc72779084eaf919f23ae9d40e867c3
Jamon Camisso <jamonation+git@gmail.com>
2022-08-26 15:02:00 -0400

    Tidy up terminal CSS and JS files

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -1,15 +1,14 @@
-var now = new Date().getTime();
-var end = now + (1000 * 60 * 60);
-var interval = null;
+let now = new Date().getTime();
+let interval = null;
+const end = now + (1000 * 60 * 60);
 
-function timer() {
-  var t = end - now;
-  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((t % (1000 * 60)) / 1000);
-  document.getElementById('timer').innerHTML = minutes + 'm ' + seconds + 's ';
+function timer () {
+  const t = end - now;
+  const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  document.getElementById('timer').innerHTML = minutes + 'm remaining';
   now = now + 1000;
-  if (t == 1000 * 60 * 5) { // 5 minutes, 300 seconds
-    alert('Terminal will close in 5 minutes')
+  if (t === 1000 * 60 * 5) { // 5 minutes, 300 seconds
+    alert('Terminal will close in 5 minutes');
   }
   if (t <= 0) {
     window.clearInterval(interval);
@@ -20,20 +19,18 @@ function timer() {
 
 (function () {
   window.terminal = this;
-
   window.terminal.load = () => {
-    let t = document.getElementById('terminal-init'),
-      image = t.attributes.getNamedItem('data-image').nodeValue,
-      iframe = `<div id="terminal-container"><iframe id="terminal" frameBorder="0" rel="opener" src="https://terminal.inky.wtf/?image=${image}"></iframe></div>`;
+    const t = document.getElementById('terminal-init');
+    const image = t.attributes.getNamedItem('data-image').nodeValue;
+    const iframe = `<div id="terminal-container"><iframe id="terminal" frameBorder="0" rel="opener" src="https://terminal.inky.wtf/?image=${image}"></iframe></div>`;
     t.insertAdjacentHTML('afterend', iframe);
     interval = window.setInterval(timer, 1000);
-  };
+  }
 
   window.terminal.exit = () => {
     document.getElementById('terminal-container').remove();
     document.getElementById('terminal-nav').remove();
-  };
-
+  }
 })();
 
 window.terminal.load();
