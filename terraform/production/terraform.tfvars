6c3e79198e0102511356e241a17ffeb6bd9c3ca8
Jamon Camisso <jamonation+git@gmail.com>
2022-08-31 15:29:48 -0400

    Add terraform instructions, and staging and production resource files

---
+++ terraform/production/terraform.tfvars
@@ -0,0 +1,2 @@
+billing_account = "012103-5BED11-316D04"
+project_id = "chainguard-academy"
---
+++ terraform/production/variables.tf
@@ -0,0 +1,6 @@
+variable project_id {}
+
+variable billing_account {
+  description = "Billing account ID"
+}
+
