303ff4653fc3d84b7b644ff85f1791fcdfc86553
Garry Ing <garry@chainguard.dev>
2023-10-13 14:46:00 -0400

    adding scroll position helper (#1067)

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,6 +103,20 @@ Array.from(searchForms).forEach(container => {
   }
 });
 
+const menuPositioner = () => {
+  const nav = document.querySelectorAll("#sidebar-default")[1]
+  const itemActive = document.querySelectorAll(".sidebar-item-active")
+  const itemActiveMobile = document.querySelectorAll(".offcanvas-body .sidebar-item-active")
+  const navMobile = document.querySelector(".offcanvas-body")
+
+  if (itemActive.length) {
+    nav.scrollTop = itemActive[itemActive.length - 1].offsetTop - 300
+    navMobile.scrollTop = itemActiveMobile[itemActiveMobile.length - 1].offsetTop - 200
+  }
+}
+
+menuPositioner()
+
 window.addEventListener('load', function() {
   index = new FlexSearch.Document({
     tokenize: "forward",
