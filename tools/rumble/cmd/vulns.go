chainguard-dev/edu

commit 1072ff4c4f5186a160b5bfee17ce8a95f707c927

perf: Query BigQuery once per day not 8000 times (#2790)
BigQuery does a full table scan unless we select partitions. We're
currently querying the `rumble_vulns` view once per vulnerability, which
is quadratically expensive and costing us over $500 per day.

This changes the job to do one query, and moves the grouping logic into
Go.

Since everything is already entirely in memory already, I don't expect
this to cause OOMs.

We still query the local SQLite table once per vulnerability, since it
works, doesn't show up on our cost bill, and I want to keep this fairly
contained.

```sql
with 

base as (select * from `region-us.INFORMATION_SCHEMA.JOBS` where date(creation_time) = '2025-11-23')

select
  substr(query, 40),
  count(*) as num_queries,
  sum(total_bytes_billed) as total_bytes_billed,
  6 * sum(total_bytes_billed) / (pow(1024, 4)) as cost
from base
where user_email = 'github-chainguard-academy@chainguard-academy.iam.gserviceaccount.com'
group by 1
order by 4 desc
```

| query | num_queries |total_bytes_billed | cost |
| --- | --: | --: | --: |
| SELECT scan.image, scan.time as time | 7459 |97762042445824 |
533.4843578338623 |
| WITH ruuuumble AS ( SELECT scan.image, | 36 | 704013926400 |
3.8417816162109375 |
| SELECT DISTINCT vulnerability FROM `clo" | 1 | 13106151424 |
0.071519851684570312 |
| SELECT ROW_NUMBER() OVER (ORDER BY tim | 1 | 1669332992 |
0.0091094970703125 |

## Type of change

Performance optimization.

### What should this PR do?

Reduce the number of identical BigQuery queries.

### Why are we making this change?

To reduce BigQuery costs.

### How should this PR be tested?

After we merge the PR, we should trigger a manual run of the Rumble
Vulnerability Data workflow.

- [ ] Does it succeed?
- [ ] Does it generate plausibly correct data? (Someone other than me
will have to check this)
- [ ] Does it result in fewer queries and fewer total bytes billed?
(I'll check the BigQuery logs)

---------

Signed-off-by: Jonathan Lange <jml@mumak.net>

tools/rumble/cmd/vulns.go
@@ -14,12 +14,10 @@ import (
 	"os"
 	"strings"
 
-	"cloud.google.com/go/bigquery"
 	cgbigquery "github.com/chainguard-dev/edu/tools/rumble/pkg/bigquery"
 	"github.com/chainguard-dev/edu/tools/rumble/pkg/grype"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
-	"google.golang.org/api/iterator"
 )
 
 type vulnsJson struct {
@@ -73,20 +71,54 @@ func (v *vulnsJson) generateJSON() error {
 	}
 	defer closer()
 
-	q := v.bqClient.Client.Query(cgbigquery.AllVulnsQuery)
-	allVulnRecords, err := v.bqClient.Query(q, cgbigquery.CveQueryType)
+	// Single query to get all vulnerability-image relationships
+	q := v.bqClient.Client.Query(cgbigquery.AllVulnsWithImagesQuery)
+	allRecords, err := v.bqClient.Query(q, cgbigquery.VulnWithImagesQueryType)
 	if err != nil {
-		log.Fatalf("error performing AllVulnsQuery: %v", err)
+		log.Fatalf("error performing AllVulnsWithImagesQuery: %v", err)
 	}
 
-	v.vulns, err = v.grypeDb.ExtractCVEs(allVulnRecords)
+	// Group results by vulnerability ID
+	vulnImageMap := make(map[string][]cgbigquery.VulnWithImages)
+	for _, record := range allRecords {
+		vwi := record.(*cgbigquery.VulnWithImages)
+		vulnImageMap[vwi.Vulnerability] = append(vulnImageMap[vwi.Vulnerability], *vwi)
+	}
+
+	// Extract unique CVE IDs for enrichment
+	cveRecords := make([]interface{}, 0, len(vulnImageMap))
+	for vulnID := range vulnImageMap {
+		cveRecords = append(cveRecords, &grype.Cve{Vulnerability: vulnID})
+	}
+
+	// Enrich with Grype metadata from local SQLite DB
+	v.vulns, err = v.grypeDb.ExtractCVEs(cveRecords)
 	if err != nil {
 		log.Fatalf("error querying grype DB: %v", err)
 	}
 
-	err = v.queryAffectedImages()
-	if err != nil {
-		log.Fatalf("error correlating vulnerabilities and images: %v", err)
+	// Populate image data for each vulnerability
+	for i := range v.vulns {
+		vulnID := v.vulns[i].Id
+		imageRecords := vulnImageMap[vulnID]
+
+		// Group by image to aggregate dates
+		imagesMap := make(map[string]grype.VulnImage)
+		for _, record := range imageRecords {
+			img := imagesMap[record.Image]
+			img.Dates = append(img.Dates, record.Time)
+			img.Image = record.Image
+			imagesMap[record.Image] = img
+		}
+
+		// Split into Chainguard vs External
+		for _, img := range imagesMap {
+			if strings.Contains(img.Image, "cgr.dev") {
+				v.vulns[i].Chainguard = append(v.vulns[i].Chainguard, img)
+			} else {
+				v.vulns[i].External = append(v.vulns[i].External, img)
+			}
+		}
 	}
 
 	switch v.opts.upload {
@@ -127,65 +159,4 @@ func (v *vulnsJson) saveFiles(vulns []grype.Vuln) error {
 		})
 	}
 	return eg.Wait()
-}
-
-func (v *vulnsJson) queryAffectedImages() error {
-	eg := new(errgroup.Group)
-	eg.SetLimit(50)
-	for idx, vuln := range v.vulns {
-		vulnerability := vuln
-		i := idx
-		eg.Go(func() error {
-			log.Printf("querying %v", vulnerability.Id)
-			q := v.bqClient.Client.Query(cgbigquery.AffectedImagesQuery)
-			q.DefaultProjectID = v.bqClient.Client.Project()
-			q.Parameters = []bigquery.QueryParameter{
-				{
-					Name: "vulnerability",
-					Value: &bigquery.QueryParameterValue{
-						Type: bigquery.StandardSQLDataType{
-							TypeKind: "STRING",
-						},
-						Value: vulnerability.Id,
-					},
-				},
-			}
-
-			it, err := q.Read(v.bqClient.Ctx)
-			if err != nil {
-				return fmt.Errorf("%v", err)
-			}
-
-			imagesMap := make(map[string]grype.VulnImage)
-			for {
-				res := &grype.VulnImage{}
-				err := it.Next(res)
-				if err == iterator.Done {
-					break
-				}
-				if err != nil {
-					return fmt.Errorf("%v", err)
-				}
-				img := imagesMap[res.Image]
-				img.Dates = append(img.Dates, res.Time)
-				img.Image = res.Image
-				imagesMap[res.Image] = img
-			}
-
-			for _, img := range imagesMap {
-				if strings.Contains(img.Image, "cgr.dev") {
-					v.vulns[i].Chainguard = append(v.vulns[i].Chainguard, img)
-				} else {
-					v.vulns[i].External = append(v.vulns[i].External, img)
-				}
-			}
-
-			return nil
-		})
-	}
-
-	if err := eg.Wait(); err == nil {
-		log.Println("Successfully saved all vulnerabilities.")
-	}
-	return nil
 }

tools/rumble/pkg/bigquery/bigquery.go
@@ -15,9 +15,10 @@ import (
 )
 
 const (
-	CveQueryType        = "cve"
-	ImageScanQueryType  = "scan"
-	LegacyScanQueryType = "legacyscan"
+	CveQueryType           = "cve"
+	ImageScanQueryType     = "scan"
+	LegacyScanQueryType    = "legacyscan"
+	VulnWithImagesQueryType = "vulnwithimages"
 )
 
 type BqClient struct {
@@ -48,6 +49,12 @@ type ImageScan struct {
 	Version       string
 	Type          string
 	Severity      string
+}
+
+type VulnWithImages struct {
+	Vulnerability string
+	Image         string
+	Time          string
 }
 
 func NewBqClient(project, db string) (BqClient, error) {
@@ -84,6 +91,8 @@ func (b *BqClient) Query(q *bigquery.Query, queryType string) ([]interface{}, er
 			values = &LegacyScan{}
 		case CveQueryType:
 			values = &grype.Cve{}
+		case VulnWithImagesQueryType:
+			values = &VulnWithImages{}
 		}
 		err := it.Next(values)
 		if err == iterator.Done {

tools/rumble/pkg/bigquery/queries.go
@@ -26,6 +26,17 @@ GROUP BY scan.time, scan.image
 ORDER BY scan.image, scan.time
 `
 
+	AllVulnsWithImagesQuery = `
+SELECT
+  vulns.vulnerability,
+  scan.image,
+  scan.time
+FROM ` + vulnsTable + ` AS vulns
+INNER JOIN ` + summaryTable + ` AS scan
+  ON scan.id = vulns.scan_id
+ORDER BY vulns.vulnerability, scan.image, scan.time
+`
+
 	LegacyCsvQuery = `
 SELECT
 	ROW_NUMBER() OVER (ORDER BY time),

commit 86d0d3253a26a39e05fcabe70f5262e547facc61

fix: Correct type for time (#2798)
[x] Check if this is a typo or other quick fix and ignore the rest :)

Fixes issue introduced by
https://github.com/chainguard-dev/edu/pull/2790

tools/rumble/cmd/vulns.go
@@ -106,7 +106,7 @@ func (v *vulnsJson) generateJSON() error {
 		imagesMap := make(map[string]grype.VulnImage)
 		for _, record := range imageRecords {
 			img := imagesMap[record.Image]
-			img.Dates = append(img.Dates, record.Time)
+			img.Dates = append(img.Dates, record.Time.Format("2006-01-02"))
 			img.Image = record.Image
 			imagesMap[record.Image] = img
 		}

tools/rumble/pkg/bigquery/bigquery.go
@@ -8,6 +8,7 @@ package cgbigquery
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"cloud.google.com/go/bigquery"
 	"github.com/chainguard-dev/edu/tools/rumble/pkg/grype"
@@ -54,7 +55,7 @@ type ImageScan struct {
 type VulnWithImages struct {
 	Vulnerability string
 	Image         string
-	Time          string
+	Time          time.Time
 }
 
 func NewBqClient(project, db string) (BqClient, error) {