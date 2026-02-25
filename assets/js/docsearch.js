510f0f3816f113711bc97c4c5d233d308aed09b1
s-stumbo <sally.stumbo@chainguard.dev>
2026-02-25 15:44:31 -0500

    fix attempt
    Signed-off-by: s-stumbo <sally.stumbo@chainguard.dev>

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -1,16 +1,9 @@
 import docsearch from "@docsearch/js";
 
 docsearch({
-  container: "#docsearch",
+  container: ".is-search",
   appId: "P6WD6RQSFQ",          
   indexName: "chainguard",  
   apiKey: "9846ce061788834124713a47b1cfd2f7", // the Search-Only key (not admin)
   insights: true,
 });
-
-const onClick = function () {
-  document.getElementsByClassName("DocSearch-Button")[0].click();
-};
-
-document.getElementById("searchToggleMobile").onclick = onClick;
-document.getElementById("searchToggleDesktop").onclick = onClick;
