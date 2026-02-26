90ffad8b477bf10a0aeb4ee642ab627b34968838
s-stumbo <sally.stumbo@chainguard.dev>
2026-02-26 10:53:45 -0500

    add search bar attempt
    Signed-off-by: s-stumbo <sally.stumbo@chainguard.dev>

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -6,3 +6,13 @@ docsearch({
   indexName: "chainguard",
   apiKey: "9846ce061788834124713a47b1cfd2f7",
 });
+
+function openDocSearch() {
+  const btn = document.querySelector(".DocSearch-Button");
+  if (btn) btn.click();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const trigger = document.getElementById("searchToggleDesktop");
+  if (trigger) trigger.addEventListener("click", openDocSearch);
+});
