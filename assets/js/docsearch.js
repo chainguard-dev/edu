4676d9f6926233e0ec62cd5b93444f84b6836111
Garry Ing <garry@chainguard.dev>
2026-03-24 10:26:10 -0400

    fix docsearch theme and section heading spacing

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -5,7 +5,7 @@ docsearch({
   appId: "P6WD6RQSFQ",
   indexName: "chainguard",
   apiKey: "9846ce061788834124713a47b1cfd2f7",
-  theme: document.documentElement.hasAttribute("data-dark-mode") ? "dark" : "light",
+  theme: localStorage.getItem("theme") === "dark" ? "dark" : "light",
 });
 
 function openDocSearch() {
